3294d5608e59dbac692aa77fd1642cc1f8d5da70
Avi Avinav <aviavinav2004@gmail.com>
2022-02-27 17:05:30 +0530

    exported everything

---
+++ main.ts
@@ -0,0 +1,6 @@
+import copyToClipboard from "./functions/copyToClipboard";
+import isDarkMode from "./functions/isDarkMode";
+import scrollToTop from "./functions/scrollToTop";
+import scrollToBottom from "./functions/scrollToBottom";
+
+export { copyToClipboard, isDarkMode, scrollToTop, scrollToBottom };
