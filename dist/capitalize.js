eafad3b01b844083e030b1e63a1bd14736fd4e9d
Avi Avinav <aviavinav2004@gmail.com>
2022-02-28 18:40:12 +0530

    removed dist folder

--- dist/capitalize.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-exports.default = capitalize;
--- dist/scrollToTop.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const scrollToTop = (element) => element.scrollIntoView({ behavior: "smooth", block: "start" });
-exports.default = scrollToTop;
