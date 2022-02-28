3f13de5a8aedaaabccd9f884c2cd94625194b893
Avi Avinav <aviavinav2004@gmail.com>
2022-02-28 15:37:54 +0530

    add capitalize function

--- main.ts
+++ main.ts
@@ -3,5 +3,6 @@ import isDarkMode from "./functions/isDarkMode";
 import scrollToTop from "./functions/scrollToTop";
 import scrollToBottom from "./functions/scrollToBottom";
 import generateRandomHexColor from "./functions/generateRandomHex";
+import capitalize from "./functions/capitalize";
 
-export { copyToClipboard, isDarkMode, scrollToTop, scrollToBottom, generateRandomHexColor };
+export { copyToClipboard, isDarkMode, scrollToTop, scrollToBottom, generateRandomHexColor, capitalize };
